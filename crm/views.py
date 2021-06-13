cc479ae0630ef2338ede452683c6e7bb61f40cc4
visavin <visavin@yandex.ru>
2021-06-13 20:41:51 +0700

    Django_HTML_form

--- crm/views.py
+++ crm/views.py
@@ -6,3 +6,12 @@ from .models import Order
 def first_page(request):
     object_list = Order.objects.all()
     return render(request, './index.html', {'object_list': object_list})
+
+
+def thanks_page(request):
+    name = request.POST['name']
+    phone = request.POST['phone']
+    element = Order(order_name=name, order_phone=phone)
+    element.save()
+    return render(request, './thanks_page.html', {'name': name, 'phone': phone})
+
