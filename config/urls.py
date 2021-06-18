c9b0fe06c923b16b983550afb8545f5c73e020f2
visavin <visavin@yandex.ru>
2021-06-18 17:34:20 +0700

    Django_landing_page_with_custom_admin (deploy_Full)

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,4 @@
-"""landingpage URL Configuration
+"""config URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -18,10 +18,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from crm import views
+from .settings import DEBUG
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('', views.first_page),
-                  path('thanks/', views.thanks_page, name='thanks_page')
-              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)\
-              + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('admin/', admin.site.urls),
+    path('', views.first_page),
+    path('thanks/', views.thanks_page, name='thanks_page')
+]
+
+if DEBUG:
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+                  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
